86ebcbc72a89f89d3a9e95da1fae6ecd36e6cd7a
dnacu <winteri1276@gmail.com>
2020-11-23 13:22:31 +0900

    chore: add dummy pets data

--- server/src/db.ts
+++ server/src/db.ts
@@ -5,7 +5,87 @@ let collections = {
   pets: [
     {
       id: "pet_wqf0rv0im6",
-      name: "댕댕이",
+      name: "댕댕이100",
+    },
+    {
+      id: "pet_wqf0rv0im2",
+      name: "댕댕이2",
+    },
+    {
+      id: "pet_wqf0rv0im3",
+      name: "댕댕이3",
+    },
+    {
+      id: "pet_wqf0rv0im4",
+      name: "댕댕이4",
+    },
+    {
+      id: "pet_wqf0rv0im5",
+      name: "댕댕이5",
+    },
+    {
+      id: "pet_wqf0rv0i24",
+      name: "댕댕이6",
+    },
+    {
+      id: "pet_wqf0rv0im7",
+      name: "댕댕이7",
+    },
+    {
+      id: "pet_wqf0rv0im8",
+      name: "댕댕이8",
+    },
+    {
+      id: "pet_wqf0rv0im9",
+      name: "댕댕이9",
+    },
+    {
+      id: "pet_wqf0rv0i10",
+      name: "댕댕이10",
+    },
+    {
+      id: "pet_wqf0rv0i11",
+      name: "댕댕이11",
+    },
+    {
+      id: "pet_wqf0rv0i12",
+      name: "댕댕이12",
+    },
+    {
+      id: "pet_wqf0rv0i13",
+      name: "댕댕이13",
+    },
+    {
+      id: "pet_wqf0rv0i14",
+      name: "댕댕이14",
+    },
+    {
+      id: "pet_wqf0rv0i15",
+      name: "댕댕이15",
+    },
+    {
+      id: "pet_wqf0rv0i16",
+      name: "댕댕이16",
+    },
+    {
+      id: "pet_wqf0rv0i17",
+      name: "댕댕이17",
+    },
+    {
+      id: "pet_wqf0rv0i18",
+      name: "댕댕이18",
+    },
+    {
+      id: "pet_wqf0rv0i19",
+      name: "댕댕이19",
+    },
+    {
+      id: "pet_wqf0rv0i20",
+      name: "댕댕이20",
+    },
+    {
+      id: "pet_wqf0rv0i21",
+      name: "댕댕이21",
     },
   ] as Pet[],
 };
