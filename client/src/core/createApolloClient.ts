17dc1666857a56161cc36e7242c5b14b8797fc4c
dnacu <winteri1276@gmail.com>
2020-11-23 16:51:03 +0900

    feat: define product field cache strategy - keyArgs, merge 에 대하여 정의함으로써, pagination된 query를 통해 얻은 데이터를 캐시에 차례대로 쌓음

--- client/src/core/createApolloClient.ts
+++ client/src/core/createApolloClient.ts
@@ -1,8 +1,25 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { offsetLimitPagination } from "@apollo/client/utilities";
 
 export function createApolloClient() {
   const client = new ApolloClient({
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            // offsetLimitPagination 동작 구현
+            products: {
+              // 결과값을 캐시데이터에 분리해서 저장할 것인가?
+              keyArgs: false,
+              // 새로운 값이 들어왔을 때 캐시에 저장할 방식에 대한 정의
+              merge(existing = [], incoming) {
+                return [...existing, ...incoming];
+              },
+            },
+          },
+        },
+      },
+    }),
     link: new HttpLink({
       uri: "http://localhost:4000",
     }),
